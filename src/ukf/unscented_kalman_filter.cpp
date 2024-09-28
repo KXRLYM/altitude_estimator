83a492334ab47853c129963c6e317e0df737d20e
Karlym Nam <karlym5@gmail.com>
2024-09-28 22:27:38 +1000

    vanila kalman filter with visualisation

--- src/ukf/unscented_kalman_filter.cpp
+++ src/ukf/unscented_kalman_filter.cpp
@@ -7,61 +7,184 @@
 
 #include "cnpy.h"
 #include "unscented_kalman_filter.h"
+#include "matplot/matplot.h"
 
 
 UKF::UKF() {
 
     /* ------------ read in files for demo --------------*/
+
     UKF::process_sensor_data();
 
-    /* ------------ set parameters --------------*/
+    /* ------------ set parameters for Kalman filters --------------*/
 
-    x_ = Eigen::VectorXd(1); // state vector [altitude]
+    dim_ = 1; // dimension of a state vector
 
-    std_z_ = 2; // process noise  [m]
+    x_ = Eigen::VectorXd(dim_); // state vector [altitude z]
 
-    P_ = std::pow(std_z_, 2); // (co)variance (matrix) for process noise
+    std_z_ = 20; // process noise [m]
 
-    std_gps_z_ = 0.5; // standard deviation for GPS [m]
+    std_gps_z_ = 2; // standard deviation for GPS [m]
 
-    std_sensor_z_ = 0.5; // standard deviation for proximity sensor [m]
+    std_sensor_z_ = 1; // standard deviation for proximity sensor [m]
+
+    F_ = Eigen::MatrixXd(dim_, dim_); // state transition matrix
+
+    P_ = Eigen::MatrixXd(dim_, dim_); // state covariance matrix
+
+    Q_ = Eigen::MatrixXd(dim_, dim_); // process covariance matrix
     
+    R_gps_ = Eigen::MatrixXd(dim_, dim_); // noise covariance matrix for gps
+
+    R_prox_ = Eigen::MatrixXd(dim_, dim_); // noise covariance matrix for proximity sensor
 
 }
 
 void UKF::run_simulation() {
-    // run the unscented Kalman filter with dummy data
-}
-        
+    /*  runs the unscented Kalman filter with dummy data and plots the output */
     
-void UKF::process_sensor_data() {
-        // read 2 sensor data files - GPS and proximity sensor and store them as class variables
+    // initialise the simulation time step
+    size_t current_time_step(0);
 
-        std::string file_path_gps = std::string(DATA_DIR)+ "cootha_gps_noise.npy";
-        std::string file_path_sensor = std::string(DATA_DIR)+ "cootha_sensor_noise.npy";
+    // set the initial state to be the average of two values
+    x_(current_time_step) = (get_gps_measurement(current_time_step) + get_prox_sensor_measurement(current_time_step)) / 2; 
 
-        cnpy::NpyArray gps_arr = cnpy::npy_load(file_path_gps);
-        cnpy::NpyArray sensor_arr = cnpy::npy_load(file_path_sensor);
+    // initialise a state covariance matrix with a std of 10m
+    P_(0) = 100; 
 
-        double* gps_data_ptr = gps_arr.data<double>();
-        double* sensor_data_ptr = sensor_arr.data<double>();
+    // construct the process covariance matrix
+    Q_(0) = std::pow(std_z_, 2);
 
-        // assume the same sized array for both sensors
-        size_t size = gps_arr.shape[0];
+    while (current_time_step < simulation_end_time_) {
+        double z_gps = get_gps_measurement(current_time_step);
+        double z_prox = get_prox_sensor_measurement(current_time_step);
+
+        predict(); 
+        correct(z_gps, z_prox); 
+
+        results_(current_time_step) = x_(0);
+        current_time_step++;
+    }
+
+    // plot the final result
+    plot();
+
+    
+}
+
+double UKF::get_gps_measurement(size_t time_step) {
+    // normally sensor read from the actual running hardware
+    return gps_data_[time_step];
+}
+
+double UKF::get_prox_sensor_measurement(size_t time_step) {
+    // normally sensor read from the actual running hardware
+    return sensor_data_[time_step];
+}
+
         
-        UKF::gps_data_.resize(size);
-        UKF::sensor_data_.resize(size);
+void UKF::process_sensor_data() {
+    // read 2 sensor data files - GPS and proximity sensor and store them as class variables
 
-        gps_data_ = Eigen::Map<Eigen::VectorXd>(gps_data_ptr, size);
-        sensor_data_ = Eigen::Map<Eigen::VectorXd>(sensor_data_ptr, size);
+    std::string file_path_gps = std::string(DATA_DIR)+ "cootha_gps_noise.npy";
+    std::string file_path_sensor = std::string(DATA_DIR)+ "cootha_sensor_noise.npy";
+
+    cnpy::NpyArray gps_arr = cnpy::npy_load(file_path_gps);
+    cnpy::NpyArray sensor_arr = cnpy::npy_load(file_path_sensor);
+
+    double* gps_data_ptr = gps_arr.data<double>();
+    double* sensor_data_ptr = sensor_arr.data<double>();
+
+    // assume the same sized array for both sensors
+    size_t size = gps_arr.shape[0];
+    
+    UKF::gps_data_.resize(size);
+    UKF::sensor_data_.resize(size);
+
+    results_.resize(size);
+
+    gps_data_ = Eigen::Map<Eigen::VectorXd>(gps_data_ptr, size);
+    sensor_data_ = Eigen::Map<Eigen::VectorXd>(sensor_data_ptr, size);
+
+    simulation_end_time_ = static_cast<int>(size);
 }
 
 
-void UKF::prediction() {
+void UKF::predict() {
+    /* Prediction stage of the Kalman filter, updates x_ and P_ */
+
+    
+    // construct the process covariance matrix
+    Eigen::MatrixXd P_new_(dim_, dim_);
+
+    // constrct the transition matrix - using a static model 
+    F_ << 1; 
+
+    // new state update using a static model
+    x_ = F_*x_;
+
+    // Propagate the process noise
+    P_new_ << (F_*P_*F_.transpose() + Q_);
+
+    // update the state covariance
+    P_ = P_new_;
 
 }
+
+void UKF::correct(double gps_data, double prox_data) {
+    /* Prediction stage of the Kalman filter, updates x_ and P_ */
+
+    Eigen::MatrixXd S_gps = P_ + R_gps_; // S = P + R_gps
+    Eigen::MatrixXd K_gps = P_ * S_gps.inverse(); // K_gps = P * S^(-1)
+
+    Eigen::MatrixXd S_prox = P_ + R_prox_; // S = P + R_prox
+    Eigen::MatrixXd K_prox = P_ * S_prox.inverse(); // K_prox = P * S^(-1)
+
+    Eigen::VectorXd innovation_gps = Eigen::VectorXd::Constant(dim_, gps_data) - x_; // Subtract x_ from gps_data
+    Eigen::VectorXd innovation_prox = Eigen::VectorXd::Constant(dim_, prox_data) - x_; // Subtract x_ from prox_data
+
+    Eigen::MatrixXd x_gps_ = K_gps * innovation_gps;
+    Eigen::MatrixXd x_prox_ = K_prox * innovation_prox;
+
+    x_ += ((x_gps_ + x_prox_) / 2);
+
+    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(P_.rows(), P_.cols());
+    Eigen::MatrixXd P_new_ = (I - K_gps) * P_; 
+
+    P_ = P_new_;
+   
+}
+
 
 void UKF::plot() {
+    std::cout << results_.transpose() << std::endl;
+    std::vector<double> results(results_.data(), results_.data() + results_.size());
+    std::vector<double> gps(gps_data_.data(), gps_data_.data() + gps_data_.size());
+    std::vector<double> prox(sensor_data_.data(), sensor_data_.data() + sensor_data_.size());
+
+    matplot::figure();
+    matplot::hold(matplot::on);
+    
+    // Plot each dataset and set colors
+    auto line_results = matplot::plot(results);
+    line_results->color("blue");
+
+    auto line_gps = matplot::plot(gps);
+    line_gps->color("red");
+
+
+    auto line_prox = matplot::plot(prox);
+    line_prox->color("green");
+
+
+    // Add legend
+    matplot::legend({"Kalman Filter","GPS","Proximity Sensor"});
+
+    // Show the plot
+    matplot::title("Altitude State Estimation Using a Kalman Filter");
+    matplot::xlabel("Timestep");
+    matplot::ylabel("Altitude [m]");
+    matplot::show();
 
 }
 
--- src/ukf/unscented_kalman_filter.h
+++ src/ukf/unscented_kalman_filter.h
@@ -6,16 +6,26 @@
 class UKF {
     public:
         UKF();
+
+        int dim_; // state vector dimension
         
         Eigen::VectorXd x_; // state vector [altitude]
 
         double std_z_; // standard deviation for process noise [m]
 
-        double P_; // (co)variance (matrix) for process noise 
-
         double std_gps_z_; // standard deviation for gps sensor [m]
 
         double std_sensor_z_; // standard deviation for proximity sensor noise [m]
+
+        Eigen::MatrixXd F_; // state transition matrix
+
+        Eigen::MatrixXd Q_; // process covariance matrix
+
+        Eigen::MatrixXd P_; // state covariance matrix
+
+        Eigen::MatrixXd R_gps_; // measurement noise matrix for gps
+
+        Eigen::MatrixXd R_prox_; // measurement noise matrix for proximity sensor
 
 
         void run_simulation();
@@ -23,10 +33,18 @@ class UKF {
     private: 
         Eigen::VectorXd sensor_data_;
         Eigen::VectorXd gps_data_;
+        size_t simulation_end_time_;
+        Eigen::VectorXd results_;
 
         void process_sensor_data();
 
-        void prediction();
+        double get_gps_measurement(size_t time_step);
+
+        double get_prox_sensor_measurement(size_t time_step);
+
+        void predict();
+
+        void correct(double gps_data, double prox_data);
 
         void plot();
 };
